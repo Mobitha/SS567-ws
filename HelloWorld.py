7619d7303d424d8829915beed93195eec07f9cd6
Mobitha <32204097+Mobitha@users.noreply.github.com>
2019-10-22 14:49:00 -0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+class Homework0:
+
+    def __init__(self):
+        self.output = 'hello world'
+
+    def print_hello_world(self):
+        print(self.output)
+
+
+def main():
+    Homework0().print_hello_world()
+
+
+if __name__ == '__main__':
+    main()
